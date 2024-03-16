543c7c4ccce78afc46570665a9062a702fcefe1d
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-03-17 02:40:12 +0700

    add project demo

--- cmd.sh
+++ cmd.sh
@@ -1,12 +1,10 @@
-terraform destroy -auto-approve
-terraform plan -out plan.tfplan
-terraform apply -auto-approve plan.tfplan
-
-cat /var/log/cloud-init-output.log
-tail -50 /var/log/cloud-init-output.log
-
+scp -i "cloud_act.pem" -r ./midterm/infrastructure  ec2-user@52.39.91.142:/home/ec2-user
+ssh -i "cloud_act.pem" ec2-user@52.39.91.142
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
-
-scp -i "cloud_act.pem" -r ./midterm/infrastructure  ec2-user@54.149.121.233:/home/ec2-user
+cd infrastructure
+terraform init
+terraform plan -out plan.tfplan
+terraform apply -auto-approve plan.tfplan
+terraform destroy -auto-approve

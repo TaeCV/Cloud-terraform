a8c462396866ec2c438472aabd26230de65b14ec
Chayakorn Vongbunsin <taecv.work@gmail.com>
2024-03-20 14:06:17 +0700

    finishing touches

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,5 @@
-scp -i "cloud_act.pem" -r ./midterm/infrastructure  ec2-user@52.39.91.142:/home/ec2-user
-ssh -i "cloud_act.pem" ec2-user@52.39.91.142
+scp -i "cloud_act.pem" -r ./midterm/infrastructure  ec2-user@35.87.117.26:/home/ec2-user
+ssh -i "cloud_act.pem" ec2-user@35.87.117.26
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
